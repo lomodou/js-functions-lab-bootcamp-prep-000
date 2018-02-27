fe108d3e347634e1275c4088fdc54a2050c723dd
Modou Lo <modoulo99@icloud.com>
2018-02-27 07:55:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(days, name) {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`;
 }
