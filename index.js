2a554c754b8868942ecbe2312c72569b482ec291
Modou Lo <modoulo99@icloud.com>
2018-02-27 08:09:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-// function happyHolidaysTo(name) {
-//   return `Happy holidays, ${name}!`;
-// }
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
 
-// function happyHolidaysTo(holiday, name) {
-//   return `Happy ${holiday}, ${name}!`;
-// }
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
 
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
